24754e6f89dc22ce8713c4da53f86493987f70b6
ScottSewards <scott.sewards@outlook.com>
2020-12-03 10:55:38 +0000

    Content Update
    >Added simple article placeholder.

--- main.js
+++ main.js
@@ -2,31 +2,33 @@ const $ = (css, parent = document) => parent.querySelector(css);
 const $$ = (css, parent = document) => Array.from(parent.querySelectorAll(css));
 
 window.onload = function() {
+  document.body.scrollTop;
+  document.body.scrollLeft;
   showView('Swiper');
 
   document.addEventListener('keydown', function(event) {
     console.log(event.key);
     switch(event.key) {
       case '7':
-        $('#container').style.transform = 'translate(0, 0)';
+        $('main').style.transform = 'translate(0, 0)';
         break;
       case '8':
-        $('#container').style.transform = 'translate(-100vw, 0)';
+        $('main').style.transform = 'translate(-100vw, 0)';
         break;
       case '9':
-        $('#container').style.transform = 'translate(-200vw, 0)';
+        $('main').style.transform = 'translate(-200vw, 0)';
         break;
       case '4':
-        $('#container').style.transform = 'translate(0, -100vh)';
+        $('main').style.transform = 'translate(0, -100vh)';
         break;
       case '5':
-        $('#container').style.transform = 'translate(-100vw, -100vh)';
+        $('main').style.transform = 'translate(-100vw, -100vh)';
         break;
       case '6':
-        $('#container').style.transform = 'translate(-200vw, -100vh)';
+        $('main').style.transform = 'translate(-200vw, -100vh)';
         break;
       case '2':
-        $('#container').style.transform = 'translate(-100vw, -200vh)';
+        $('main').style.transform = 'translate(-100vw, -200vh)';
         break;
     }
   }, true);
@@ -43,25 +45,25 @@ function save() {
 function showView(viewName) {
   switch(viewName) {
     case 'Sign-in':
-      $('#container').style.transform = 'translate(0, 0)';
+      $('main').style.transform = 'translate(0, 0)';
       break;
     case 'Settings':
-      $('#container').style.transform = 'translate(-100vw, 0)';
+      $('main').style.transform = 'translate(-100vw, 0)';
       break;
     case 'Sign-up':
-      $('#container').style.transform = 'translate(-200vw, 0)';
+      $('main').style.transform = 'translate(-200vw, 0)';
       break;
     case 'Ignored':
-      $('#container').style.transform = 'translate(0, -100vh)';
+      $('main').style.transform = 'translate(0, -100vh)';
       break;
     case 'Swiper':
-      $('#container').style.transform = 'translate(-100vw, -100vh)';
+      $('main').style.transform = 'translate(-100vw, -100vh)';
       break;
     case 'Saved':
-      $('#container').style.transform = 'translate(-200vw, -100vh)';
+      $('main').style.transform = 'translate(-200vw, -100vh)';
       break;
     case 'Preloader':
-      $('#container').style.transform = 'translate(-100vw, -200vh)';
+      $('main').style.transform = 'translate(-100vw, -200vh)';
       break;
   }
 }
