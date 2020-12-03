018b22f8b2d7f3572521cc9949e437734d618b0c
ScottSewards <scott.sewards@outlook.com>
2020-12-03 09:48:56 +0000

    Initial Commit
    >Added index.html, main.css, main.js and temp.png.
    >Added grid to display views.
    >Added a layout and view transition.

---
+++ main.js
@@ -0,0 +1,39 @@
+const $ = (css, parent = document) => parent.querySelector(css);
+const $$ = (css, parent = document) => Array.from(parent.querySelectorAll(css));
+
+window.onload = function() {
+  document.addEventListener('keydown', function(event) {
+    console.log(event.key);
+    switch (event.key) {
+      case "7":
+
+        break;
+      case "8":
+
+        break;
+      case "9":
+
+        break;
+      case "4":
+
+        break;
+      case "5":
+
+        break;
+      case "6":
+
+        break;
+      case "2":
+
+        break;
+    }
+  }, true);
+}
+
+function ignore() {
+
+}
+
+function save() {
+
+}
